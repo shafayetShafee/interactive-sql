380c311f10c01db4b1f592dfcc5b7ee45733c00b
shafayetShafee <seaborn.sicario@gmail.com>
2023-05-23 22:50:03 +0600

    feat: added option editable

--- _extensions/interactive-sql/interactive-sql.lua
+++ _extensions/interactive-sql/interactive-sql.lua
@@ -1,6 +1,6 @@
-p = quarto.log.output
-str = pandoc.utils.stringify
 
+str = pandoc.utils.stringify
+-- p = quarto.log.output
 
 local function ensureHtmlDeps()
   quarto.doc.add_html_dependency({
@@ -20,6 +20,7 @@ local function ensureHtmlDeps()
   })
 end
 
+
 local function add_resource()
   quarto.doc.include_file(
     "in-header",
@@ -27,8 +28,6 @@ local function add_resource()
   )
 end
 
-add_resource()
-ensureHtmlDeps()
 
 function get_dbs(databases)
   return {
@@ -37,7 +36,8 @@ function get_dbs(databases)
         local db = {
           ["name"] = str(val.name),
           ["path"] = str(val.path),
-          ["class"] = str(val.name)
+          ["class"] = str(val.name),
+          ["editable"] = val.editable == false and "" or "editable"
         }
         table.insert(databases, db)
       end
@@ -53,26 +53,32 @@ function CodeBlock(cb)
   end
 end
 
-function Pandoc(doc)
-  local databases = {}
-  doc:walk(get_dbs(databases))
-  for key, val in ipairs(databases) do
-    local sqlime_db = "<sqlime-db name=\"" 
-                      .. val.name 
-                      .. "\" path=\"" 
-                      .. val.path
-                      .. "\"></sqlime-db>"
-    local sqlime_exm = "<sqlime-examples db=\""
-                       .. val.name 
-                       .. "\" selector=\"div.interactive-sql pre."
-                       .. val.class .. " code"
-                       .. "\" editable></sqlime-examples>"
-    p(sqlime_db)
-    p(sqlime_exm)
-    local sqlime_db_html = pandoc.RawInline('html', sqlime_db)
-    local sqlime_exm_html = pandoc.RawInline('html', sqlime_exm)
-    table.insert(doc.blocks, sqlime_db_html)
-    table.insert(doc.blocks, sqlime_exm_html)
+
+if quarto.doc.is_format("html:js") then
+  function Pandoc(doc)
+    
+    add_resource()
+    ensureHtmlDeps()
+    
+    local databases = {}
+    doc:walk(get_dbs(databases))
+    for key, val in ipairs(databases) do
+      -- local editable = val.editable == "false" and "" or "editable"
+      local sqlime_db = "<sqlime-db name=\"" 
+                        .. val.name 
+                        .. "\" path=\"" 
+                        .. val.path
+                        .. "\"></sqlime-db>"
+      local sqlime_exm = "<sqlime-examples db=\""
+                         .. val.name 
+                         .. "\" selector=\"div.interactive-sql pre."
+                         .. val.class .. " code"
+                         .. "\"" .. val.editable .. "></sqlime-examples>"
+      local sqlime_db_html = pandoc.RawInline('html', sqlime_db)
+      local sqlime_exm_html = pandoc.RawInline('html', sqlime_exm)
+      table.insert(doc.blocks, sqlime_db_html)
+      table.insert(doc.blocks, sqlime_exm_html)
+    end
+    return doc
   end
-  return doc
 end
