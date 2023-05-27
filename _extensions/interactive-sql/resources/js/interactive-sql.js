420be175509332cf779483b96724024cd8747398
shafayetShafee <seaborn.sicario@gmail.com>
2023-05-28 01:13:30 +0600

    feat: removed the line numbering for editable code chunk

--- _extensions/interactive-sql/resources/js/interactive-sql.js
+++ _extensions/interactive-sql/resources/js/interactive-sql.js
@@ -4,4 +4,18 @@
     let sqlime_output = el.querySelector('div.sqlime-example');
     el.appendChild(sqlime_output);
   });
+  
+  // for revealjs, removing the `numberSource` from <pre> from when 
+  // `contenteditable="true"` in <code>, so that line numbers doesnot show
+  // up.
+  let reveal_sqldiv = document.querySelectorAll('.reveal div.interactive-sql');
+  reveal_sqldiv.forEach(el => {
+    let pre = el.querySelector('pre.sourceCode');
+    let code = el.querySelector('code.sourceCode');
+    if (code.hasAttribute('contenteditable')) {
+      if (pre.classList.contains('numberSource')) {
+        pre.classList.remove('numberSource');
+      };
+    };
+  });
 })();
